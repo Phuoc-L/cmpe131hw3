d23dd07754d720394d05c12e362ffbe1266eb565
Phuoc Le <phuocle3210@gmail.com>
2021-04-18 19:31:23 -0700

    added send message and User/Messages database

--- app/models.py
+++ app/models.py
@@ -3,18 +3,26 @@ from app import db
 class User(db.Model):
     # have the following columns
     # id (int)
+    id = db.Column(db.Integer, primary_key = True)
     # author (string, unique, can't be null)
+    author = db.Column(db.String, unique = True, nullable = False)
     # message (linkd to Messages table)
-
+    message = db.relationship('Messages', backref = 'author', lazy = 'dynamic')
+    
     def __repr__(self):
-        return f'<User {self.author}>'
+        return f'<User: {self.author}>'
 
 class Messages(db.Model):
     # have the following columns
     # id (int)
+    id = db.Column(db.Integer, primary_key = True)
     # message (string, not unique, can't be null)
+    message = db.Column(db.String, unique = False, nullable = False)
     # user_id link to id (int)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     # write __repr__ that outputs
     # <Message: MESSAGE_GOES_HERE>
     # replace MESSAGE_GOES_HERE with the message
+    def __repr__(self):
+        return f'<Message: {self.message}>'
--- app/routes.py
+++ app/routes.py
@@ -6,18 +6,44 @@ from app.forms import MessageForm
 from app.models import User, Messages
 
 # add route '/' and also add the two methods to handle request: 'GET' and 'POST'
+@app.route('/', methods = ['GET', 'POST'])
 def home():
-    form=MessageForm()
+    form = MessageForm()
     if form.validate_on_submit():
         # check if user exits in database
+        user = User.query.filter_by(author = form.author.data).first()
         # if not create user and add to database
+        if user is None:
+            new_user = User(author = form.author.data)
+            db.session.add(new_user)
         # create row in Message table with user (created/found) add to ta database
+        new_message = Messages(message = form.message.data, user_id = User.query.filter_by(author = form.author.data).first().id)
+        db.session.add(new_message)
+        db.session.commit()
+    
 
-    posts = []
+    posts = [
+        {   'author':'carlos', 
+            'message':'Yo! Where you at?!'
+        },
+        {   'author':'Jerry', 
+            'message':'Home. You?'}
+    ]
+
+    
+    '''f'{Messages.query.filter_by(id = onemessage.user_id.data).first()}','''
     # output all messages
     # create a list of dictionaries with the following structure
     # [{'author':'carlos', 'message':'Yo! Where you at?!'},
     #  {'author':'Jerry', 'message':'Home. You?'}]
+    allmessages = Messages.query.all()
+    if allmessages is not None:
+        for onemessage in allmessages:
+            posts = posts + [
+            {   'author':f'{User.query.filter_by(id = onemessage.user_id).first().author}', 
+                'message':f'{onemessage.message}'}
+            ]
+   
 
     return render_template('home.html', posts=posts, form=form)
 
